23b0f2128234e535787b8028db89215447a368d2
lion-oldriver <w879443a72cj2i@gmail.com>
2021-11-07 06:31:08 +0000

    Add password reset

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -10,7 +10,9 @@ class UserMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://f3d4fd381d20490e843f24bf73f9b251.vfs.cloud9.us-east-1.amazonaws.com/rails/mailers/user_mailer/password_reset
   def password_reset
-    UserMailer.password_reset
+    user = User.first
+    user.reset_token = User.new_token
+    UserMailer.password_reset(user)
   end
 
 end
